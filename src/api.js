ff3cb71db0bde3b51e8e057155d86fa19fbd41c3
tejassrivastava <tejas.srivsatava@hotmail.com>
2023-09-09 11:56:04 +0530

    API update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Replace with your backend API URL
+  baseURL: 'https://fierce-puce-pleat.cyclic.cloud/api', // Replace with your backend API URL
 });
 
 export default instance;
